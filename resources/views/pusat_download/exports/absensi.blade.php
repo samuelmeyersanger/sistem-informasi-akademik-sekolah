9290e5194bc8cacfe8856644f8c069d2c7936c85
Samuel Meyer Sanger <samuelmeyersanger@gmail.com>
2026-07-06 10:22:14 +0700

    Fixed

--- resources/views/pusat_download/exports/absensi.blade.php
+++ resources/views/pusat_download/exports/absensi.blade.php
@@ -3,46 +3,51 @@
 <head>
     <title>Daftar Hadir</title>
     <style>
-        /* Pengaturan Margin 1 cm khusus saat dicetak/dijadikan PDF */
-        @page { 
-            margin: 1cm; 
-        }
-        /* Ukuran font diperkecil sedikit agar muat dalam potrait F4 */
+        @page { margin: 1cm; }
         body { font-family: sans-serif; font-size: 10px; }
-        
-        .table-main { width: 100%; border-collapse: collapse; margin-top: 15px; }
-        .table-main th, .table-main td { border: 1px solid #000; padding: 4px; text-align: center; }
-        .table-main .text-left { text-align: left; }
+        table { width: 100%; border-collapse: collapse; }
+        th, td { padding: 4px; }
+        .border-all th, .border-all td { border: 1px solid #000; }
+        .text-center { text-align: center; }
+        .text-left { text-align: left; }
+        .text-bold { font-weight: bold; }
         .bg-gray { background-color: #f2f2f2; }
         .bg-yellow { background-color: #fff2cc; }
-        .header-table { width: 100%; border: none; font-weight: bold; margin-bottom: 15px;}
-        .header-table td { padding: 4px; }
     </style>
 </head>
 <body>
-    <table class="header-table">
+    <table>
+        <!-- ================= HEADER AREA ================= -->
         <tr>
-            <td width="30%" style="border: 1px solid #000;" align="center">
-                <div style="font-size: 9px;">WALI KELAS</div>
-                <div class="bg-yellow" style="padding: 4px; margin-top: 4px; border: 1px solid #000;">
-                    {{ $kelas->waliKelas ? $kelas->waliKelas->nama_lengkap : 'Belum Ada Wali Kelas' }}
-                </div>
+            <td colspan="6" class="text-center text-bold" style="font-size: 9px;">WALI KELAS</td>
+            <td colspan="9"></td>
+            <td colspan="12" class="text-center text-bold" style="font-size: 13px;">DAFTAR HADIR</td>
+        </tr>
+        <tr>
+            <td colspan="6" class="text-center text-bold bg-yellow" style="border: 1px solid #000;">
+                {{ $kelas->waliKelas ? $kelas->waliKelas->nama_lengkap : 'Belum Ada Wali Kelas' }}
             </td>
-            <td width="5%"></td>
-            <td width="30%" align="center">
-                <div class="bg-yellow" style="font-size: 20px; border: 1px solid #000; padding: 4px;">{{ $kelas->nama_kelas }}</div>
+            <td colspan="3"></td>
+            <td colspan="5" class="text-center text-bold bg-yellow" style="font-size: 20px; border: 1px solid #000;">
+                {{ $kelas->nama_kelas }}
             </td>
-            <td width="5%"></td>
-            <td width="30%" align="center" style="font-size: 13px;">
-                DAFTAR HADIR <br>
-                {{ $nama_sekolah }} <br>
-                <div class="bg-yellow" style="margin-top: 4px; padding: 2px;">TAHUN AJARAN {{ $tahun_ajaran }}</div>
+            <td colspan="1"></td>
+            <td colspan="12" class="text-center text-bold" style="font-size: 13px;">
+                {{ $nama_sekolah }}
             </td>
         </tr>
-    </table>
-    <table class="table-main">
-        <thead>
-            <tr class="bg-gray">
+        <tr>
+            <td colspan="15"></td>
+            <td colspan="12" class="text-center text-bold bg-yellow" style="border: 1px solid #000;">
+                TAHUN AJARAN {{ $tahun_ajaran }}
+            </td>
+        </tr>
+        
+        <!-- SPACING -->
+        <tr><td colspan="27" style="height: 15px;"></td></tr>
+        <!-- ================= DATA AREA ================= -->
+        <tbody class="border-all text-center">
+            <tr class="bg-gray text-bold">
                 <th rowspan="2" width="3%">No</th>
                 <th rowspan="2" width="12%">NISN</th>
                 <th rowspan="2" width="25%">Nama</th>
@@ -50,8 +55,7 @@
                 <th colspan="20">Tanggal dan Bulan</th>
                 <th colspan="3">Jumlah</th>
             </tr>
-            <tr class="bg-gray">
-                <!-- Looping 20 kolom kosong untuk tanggal -->
+            <tr class="bg-gray text-bold">
                 @for($i=1; $i<=20; $i++)
                     <th width="2%"></th>
                 @endfor
@@ -59,8 +63,6 @@
                 <th width="3%">I</th>
                 <th width="3%">A</th>
             </tr>
-        </thead>
-        <tbody>
             @forelse($anggota as $index => $item)
                 <tr>
                     <td>{{ str_pad($index + 1, 2, '0', STR_PAD_LEFT) }}</td>
@@ -78,16 +80,17 @@
                 </tr>
             @endforelse
         </tbody>
-    </table>
-    <table style="width: 100%; margin-top: 15px; font-size: 11px; font-weight: bold;">
+        <!-- SPACING -->
+        <tr><td colspan="27" style="height: 15px;"></td></tr>
+        <!-- ================= FOOTER AREA ================= -->
         <tr>
-            <td width="40%"></td>
-            <td width="20%" align="center">
+            <td colspan="12"></td>
+            <td colspan="6" class="text-center text-bold">
                 JUMLAH<br>
                 Laki-laki &nbsp;&nbsp;&nbsp; {{ $laki_laki }}<br>
                 Perempuan &nbsp; {{ $perempuan }}
             </td>
-            <td width="40%"></td>
+            <td colspan="9"></td>
         </tr>
     </table>
 </body>
